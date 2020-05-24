83b1451645de2818ee0f45474b5b21a1b6e6a7fa
Ryo-Kikkawa <l.lawliet.822k@gmail.com>
2020-05-24 17:34:27 +0900

    transmission

--- Gemfile
+++ Gemfile
@@ -65,3 +65,6 @@ gem 'devise'
 gem "haml-rails", ">= 1.0", '<= 2.0.1'
 gem 'font-awesome-sass'
 gem 'pry-rails'
+
+gem 'carrierwave'
+gem 'mini_magick'
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
   has_many :group_users
   has_many :groups, through: :group_users
+  has_many :messages
        
 end
