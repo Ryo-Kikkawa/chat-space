8545696aeb9e095c67f382aeb40c10a976a4e5df
Ryo-Kikkawa <l.lawliet.822k@gmail.com>
2020-05-28 16:09:06 +0900

    inremental-search ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 
   def edit
   end
+  
 
   def update
     if current_user.update(user_params)
