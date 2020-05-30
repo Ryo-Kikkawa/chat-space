054d1104889828cd159ad9bc56d364439b4aeed2
Ryo-Kikkawa <l.lawliet.822k@gmail.com>
2020-05-30 11:25:15 +0900

    binding.pry delete

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    # binding.pry
     group = Group.find(params[:group_id])
     last_message_id = params[:id].to_i
     @messages = group.messages.includes(:user).where("id > ?", last_message_id)
